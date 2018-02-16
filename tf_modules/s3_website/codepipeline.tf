818a4ab18e1138ea6d694f46ddde2e8bc605444e
Daniel Hartnell <danielhartnell@gmail.com>
2018-02-16 13:01:35 -0800

    wip adding CodePipeline

---
+++ tf_modules/s3_website/codepipeline.tf
@@ -0,0 +1,110 @@
+resource "aws_s3_bucket" "foo" {
+  bucket = "codepipeline_${var.service_name}"
+  acl    = "private"
+}
+
+resource "aws_iam_role" "foo" {
+  name = "codepipeline_${var.service_name}_role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "codepipeline.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "codepipeline_policy" {
+  name = "codepipeline_${var.service_name}_policy"
+  role = "${aws_iam_role.codepipeline_role.id}"
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect":"Allow",
+      "Action": [
+        "s3:GetObject",
+        "s3:GetObjectVersion",
+        "s3:GetBucketVersioning"
+      ],
+      "Resource": [
+        "${aws_s3_bucket.foo.arn}",
+        "${aws_s3_bucket.foo.arn}/*"
+      ]
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "codebuild:BatchGetBuilds",
+        "codebuild:StartBuild"
+      ],
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+
+data "aws_kms_alias" "s3kmskey" {
+  name = "alias/myKmsKey"
+}
+
+resource "aws_codepipeline" "foo" {
+  name     = "tf-test-pipeline"
+  role_arn = "${aws_iam_role.foo.arn}"
+
+  artifact_store {
+    location = "${aws_s3_bucket.foo.bucket}"
+    type     = "S3"
+    encryption_key {
+      id   = "${data.aws_kms_alias.s3kmskey.arn}"
+      type = "KMS"
+    }
+  }
+
+  stage {
+    name = "Source"
+
+    action {
+      name             = "Source"
+      category         = "Source"
+      owner            = "ThirdParty"
+      provider         = "GitHub"
+      version          = "1"
+      output_artifacts = ["test"]
+
+      configuration {
+        Owner      = "mozilla"
+        Repo       = "release.mozilla.org"
+        Branch     = "gh-pages"
+      }
+    }
+  }
+
+  stage {
+    name = "Build"
+
+    action {
+      name            = "Build"
+      category        = "Build"
+      owner           = "AWS"
+      provider        = "CodeBuild"
+      input_artifacts = ["test"]
+      version         = "1"
+
+      configuration {
+        ProjectName = "${var.codebuild_project_name}"
+      }
+    }
+  }
+}
+
