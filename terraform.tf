78592faa495049f87121375b6df87c71b91ae11d
Daniel Hartnell <danielhartnell@gmail.com>
2018-02-16 09:53:22 -0800

    using serivce name to define remote state path

--- terraform.tf
+++ terraform.tf
@@ -2,6 +2,6 @@ terraform {
   backend "s3" {
     bucket = "webops-terraform-remote-state-store"
     region = "us-west-2"
-    key    = "prod/websites/static/webops-release-mozilla-org.tfstate"
+    key    = "websites/static/${var.service_name}/terraform.tfstate"
   }
 }
