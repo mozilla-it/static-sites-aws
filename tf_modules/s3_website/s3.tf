5ea79372dd4106e92177f49170883d999a140fe5
Daniel Hartnell <danielhartnell@gmail.com>
2018-02-21 15:16:18 -0800

    removed unnecessary public-read acl in S3 bucket

--- tf_modules/s3_website/s3.tf
+++ tf_modules/s3_website/s3.tf
@@ -2,7 +2,6 @@
 
 resource "aws_s3_bucket" "prod_bucket" {
   bucket = "${var.service_name}"
-  acl    = "public-read"
 
   website {
     index_document = "index.html"
@@ -19,7 +18,7 @@ resource "aws_s3_bucket" "prod_bucket" {
 # S3 bucket access log storage
 
 resource "aws_s3_bucket" "log_bucket" {
-  bucket = "${var.service_name}"
+  bucket = "${var.service_name}-logs"
   acl    = "log-delivery-write"
   tags = "${var.webops_tags}"
 }
