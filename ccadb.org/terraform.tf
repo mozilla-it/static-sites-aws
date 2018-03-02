7e8eb052f377c6df03c1da9d3d5690d4f36f8425
eziegenhorn <eric@ziegenhorns.com>
2018-03-02 09:59:09 -0600

    Initial commit of ccadb.org
    Fix typo in key

--- ccadb.org/terraform.tf
+++ ccadb.org/terraform.tf
@@ -2,6 +2,6 @@ terraform {
   backend "s3" {
     bucket  = "webops-ccadb-terraform-shared-state"
     region  = "us-east-1"
-    key     = "websites/static/ccadm_org/terraform.tfstate"
+    key     = "websites/static/ccadb_org/terraform.tfstate"
   }
 }
