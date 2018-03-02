ad3aeb889abfd1702226bf3bf902434ba4caeb98
eziegenhorn <eric@ziegenhorns.com>
2018-03-02 14:29:54 -0600

    namespace codebuild iam

--- tf_modules/s3_website/codebuild.tf
+++ tf_modules/s3_website/codebuild.tf
@@ -10,21 +10,21 @@ data "template_file" "codebuild_policy" {
 }
 
 resource "aws_iam_role" "codebuild_role" {
-  name               = "webops-codebuild-role-release-blog"
+  name               = "webops-${var.service_name}-codebuild-role"
   assume_role_policy = "${file("${path.module}/iam/codebuild-role.txt")}"
 }
 
 # IAM configuration
 
 resource "aws_iam_policy" "codebuild_policy" {
-  name        = "webops-codebuild-policy"
+  name        = "webops-${var.service_name}-codebuild-policy"
   path        = "/service-role/"
   description = "Policy used in trust relationship with CodeBuild"
   policy      = "${data.template_file.codebuild_policy.rendered}"
 }
 
 resource "aws_iam_policy_attachment" "codebuild_policy_attachment" {
-  name       = "webops-codebuild-policy-attachment"
+  name       = "webops-${var.service_name}-codebuild-policy-attachment"
   policy_arn = "${aws_iam_policy.codebuild_policy.arn}"
   roles      = ["${aws_iam_role.codebuild_role.id}"]
 }
