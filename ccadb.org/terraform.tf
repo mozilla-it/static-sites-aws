039bf92db91f94716d67da6d6704030a100fec25
eziegenhorn <eric@ziegenhorns.com>
2018-03-02 16:41:08 -0600

    Switch back to shared s3 bucket with different keys

--- ccadb.org/terraform.tf
+++ ccadb.org/terraform.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket  = "webops-ccadb-terraform-shared-state"
+    bucket  = "webops-terraform-shared-state"
     region  = "us-east-1"
     key     = "websites/static/ccadb_org/terraform.tfstate"
   }
