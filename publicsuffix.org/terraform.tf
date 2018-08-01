2b036077e7912bd5b34916b1f582f2cd3ff1c432
joeyk <jkrejci@mozilla.com>
2018-08-01 15:14:46 -0700

    Fixed bucket?

--- publicsuffix.org/terraform.tf
+++ publicsuffix.org/terraform.tf
@@ -2,6 +2,6 @@ terraform {
   backend "s3" {
     bucket  = "webops-terraform-shared-state"
     region  = "us-east-1"
-    key     = "websites/static/publicsuffix/terraform.tfstate"
+    key     = "websites/static/publicsuffixeast/terraform.tfstate"
   }
 }
