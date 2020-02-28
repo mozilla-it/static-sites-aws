b545e9cd0fe9262ef1938e39e303c5b85246cce8
limed <limed@mozilla.com>
2020-02-28 15:39:46 -0800

    Cleanup sites that has been moved to netlify

--- release.mozilla.org/terraform.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket  = "webops-terraform-shared-state"
-    region  = "us-east-1"
-    key     = "websites/static/release_mozilla_org/terraform.tfstate"
-  }
-}
